979233a47ca0b6128b496bd803899043a0684ed7
maki03117 <32335810+maki03117@users.noreply.github.com>
2020-10-14 15:13:52 +0100

    upload

--- src/components/report.js
+++ src/components/report.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
-import TutorialDataService from "../../services/class.service";
+import TutorialDataService from "../services/class.service";
 
 import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
